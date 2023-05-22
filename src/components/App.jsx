3cbd3101e80035ea87dff5d82ca87208bd573c6e
SerhiiKuhai <sergk30@gmail.com>
2023-05-22 20:49:34 +0300

    3-4baza

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import user from 'path/to/user.json';
 import data from 'path/to/data.json';
+import friends from 'path/to/friends.json';
+import transactions from 'path/to/transactions.json';
 
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -26,6 +30,8 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
